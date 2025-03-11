a2658ab9ebe111acb3edab6968a65a92b999c444
irmakguzey <irmakguzey47@gmail.com>
2025-03-11 18:12:40 -0400

    will have the first long term training

--- assignment-1/train.py
+++ assignment-1/train.py
@@ -154,7 +154,7 @@ if __name__ == "__main__":
         # results_dir="results-llamba-2025-03-11_17:11:54",
         root_dir="climate_text_dataset",
         batch_size=16,
-        num_epochs=5,
+        num_epochs=200,
         gradient_accumulation_steps=32,
         max_token_len=256,
         is_lora=True,
